fe7364651c9ca4aac28c30bff5c86f2f9e778d01
wilfredo huallpartupa <wilfredo.huallpartupa@panbox.io>
2024-02-08 17:55:17 -0500

    Se corrige error

--- src/pages/ProductDetails/index.tsx
+++ src/pages/ProductDetails/index.tsx
@@ -1,26 +1,36 @@
 import { urlDecoded } from "@/helpers";
 import { productsFake } from "@/helpers/datafake";
+import { ProductData } from "@/interfaces";
 import MainContainer from "@components/MainContainer";
 import ProductDetails from "@templates/ProductDetails";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const ProductDetailsPage = () => {
-  const { productName } = useParams();
-  console.log("aca", urlDecoded(productName));
-  //cambiar
-  const productOnParams = productsFake.filter(
-    (product) => product.name === productName
+  const params = useParams();
+  const [currentProduct, setCurrentProduct] = useState<ProductData | null>(
+    null
   );
+  useEffect(() => {
+    const productOnParams = productsFake.filter(
+      (product) => product.name === urlDecoded(params.productName)
+    );
+    if (productOnParams) {
+      setCurrentProduct(productOnParams[0]);
+    }
+  }, [params]);
 
   return (
     <MainContainer>
-      <ProductDetails
-        nameProduct={productOnParams[0].name}
-        priceProduct={productOnParams[0].price}
-        id={productOnParams[0].id}
-        imgsProduct={productOnParams[0].imgUrls}
-        variantsProduct={productOnParams[0].variants}
-      />
+      {currentProduct && (
+        <ProductDetails
+          nameProduct={currentProduct.name}
+          priceProduct={currentProduct.price}
+          id={currentProduct.id}
+          imgsProduct={currentProduct.imgUrls}
+          variantsProduct={currentProduct.variants}
+        />
+      )}
     </MainContainer>
   );
 };
